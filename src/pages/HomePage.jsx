9536afc53850f8ef50d06a031cea1e675da3c270
Makarasok24 <makarasok.it@gmail.com>
2025-04-16 00:01:57 +0700

    homepage with 2 languages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,22 +36,22 @@ const HomePage = () => {
       id: 1,
       iconBg: "bg-[#dcb9f6]",
       icon: asserts.Math,
-      title: "CIMOC Math Challenge",
-      description: "CIMOC aims to provide Cambodian students with access to high-quality mathematics competitions which helps them improve their problem-solving and logical reasoning skills."
+      title: "CIMOCMathChallenge",
+      description: "CIMOCMathChallengeDescription"
     },
     {
       id: 2,
       iconBg: "bg-[#fbe2e3]",
       icon: asserts.Brain, // Consider using a different icon if available
-      title: "Challenge Students",
-      description: "CIMOC challenges students with Olympiad-style math problems and encourages critical thinking to help students develop a deeper understanding of mathematics."
+      title: "ChallengeStudents",
+      description: "ChallengeStudentsDescription"
     },
     {
       id: 3,
       iconBg: "bg-[#8080ff]",
       icon: asserts.Support, // Consider using a different icon if available
-      title: "Support Community",
-      description: "CIMOC will allocate a portion of its funds to charity events, social initiatives, and donations to support underprivileged students by providing educational resources and financial aid to those in need."
+      title: "SupportCommunity",
+      description: "SupportCommunityDescription"
     }
   ];
   return (
@@ -63,7 +63,7 @@ const HomePage = () => {
       <ShiftingCountdown />
       {/* should put objective here as 3 grids */}
       <h3 className='mt-5 text-3xl font-semibold text-center'>
-        Our main objectives
+        {t("Ourmainobjectives")}
       </h3>
       <div className="max-w-screen-2xl grid grid-cols-3 gap-10 mt-10">
         {cardData.map((card) => (
@@ -71,8 +71,8 @@ const HomePage = () => {
             key={card.id}
             iconBg={card.iconBg}
             icon={card.icon}
-            title={card.title}
-            description={card.description}
+            title={t(card.title)}
+            description={t(card.description)}
           />
         ))}
       </div>
@@ -88,12 +88,9 @@ const HomePage = () => {
           className="mt-[5rem] p-5 gap-5 flex bg-white rounded-lg justify-center"
         >
           <span className="w-2/3">
-            <h2 className="text-3xl font-bold">What is CIMOC?</h2>
+            <h2 className="text-3xl font-bold">{t("WhatisCIMOC")}</h2>
             <p className="text-xl text-gray-500 mt-5">
-              CIMOC (Cambodia International Mathematics Olympiad Competitions) is a
-              non-profit math competition designed to provide Cambodian students
-              with an opportunity to enhance their problem-solving and logical
-              thinking skills.
+              {t("WhatisCIMOCDescription")}
             </p>
           </span>
           <motion.div animate={floatAnimation} className="p-5 bg-sContent rounded-lg">
@@ -113,12 +110,9 @@ const HomePage = () => {
             <img src={asserts.Student} className="w-100" alt="logo" />
           </motion.div>
           <span className="w-2/3">
-            <h2 className="text-3xl font-bold">Who can participate in CIMOC?</h2>
+            <h2 className="text-3xl font-bold">{t("WhoCanParticipateInCIMOC")}</h2>
             <p className="text-xl text-gray-500 mt-5">
-              CIMOC is open to all Cambodian students from Grades 1 to 12, welcoming
-              participants from both public and private schools across the country.
-              We aim to foster a love for mathematics and provide equal opportunities
-              for students of all backgrounds to challenge themselves and grow.
+              {t("WhoCanParticipateInCIMOCDescription")}
             </p>
           </span>
         </motion.div>
@@ -129,10 +123,9 @@ const HomePage = () => {
           className="mt-5 p-5 gap-5 bg-white flex justify-center rounded-lg"
         >
           <span className="w-2/3">
-            <h2 className="text-3xl font-bold">Awards & Recognitions?</h2>
+            <h2 className="text-3xl font-bold">{t("Awards&Recognitions")}</h2>
             <p className="text-xl text-gray-500 mt-5">
-              All students who compete in the final round will stand a chance to win
-              Trophies, Gold Medals, Silver Medals, Bronze Medals, and Certificates.
+              {t("Awards&RecognitionsDescription")}
             </p>
           </span>
           <motion.div animate={floatAnimation} className="p-5 bg-pContent rounded-lg">
